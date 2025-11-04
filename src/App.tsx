9bb28c770c194d101825a018f10d6641df37275d
“Muhib <muhibwaqar@hotmail.com>
2025-11-04 15:12:13 -0500

    Temporarily remove chatbot from app

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import TypingTest from "./pages/TypingTest";
 import NotFound from "./pages/NotFound";
-import Chatbot from "./components/Chatbot";
+// import Chatbot from "./components/Chatbot";
 
 const queryClient = new QueryClient();
 
@@ -23,7 +23,7 @@ const App = () => (
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
-      <Chatbot />
+  {/* <Chatbot /> */}
     </TooltipProvider>
   </QueryClientProvider>
 );
